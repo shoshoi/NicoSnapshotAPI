d16af5fbac5072d24bbecc32d2deecd3a074ee36
shoshoi <shoshoiprop@gmail.com>
2019-04-04 21:35:14 +0900

    first commit

---
+++ api_nico.rb
@@ -0,0 +1,42 @@
+require 'httpclient'
+
+class ApiNico
+  def initialize
+    @endpoint = 'https://api.search.nicovideo.jp/api/v2/snapshot/video/contents/search'
+    @user_agent = "nico-api"
+    @client = HTTPClient.new
+    @client.debug_dev = $stderr
+  end
+
+  def search_keyword(keyword)
+    query = { 
+      'q' => keyword,
+      'targets' => 'title,description,tags',
+      'fields' => 'contentId,title,viewCounter,mylistCounter,commentCounter,startTime,lengthSeconds', # option
+      '_sort' => 'startTime',
+      '_context' => 'nico-api'
+    }
+    http_get(merge_default(query))
+  end
+
+  private
+  def merge_default(query={})
+    default_query = {
+      'targets' => 'title,description,tags',
+      '_sort' => '+startTime',
+      '_context' => 'nico-api'
+    }
+    default_query.merge(query)
+  end
+
+  def http_get(query)
+    res = @client.get(@endpoint, query: query, :follow_redirect => true, default_header: {"User-Agent" => @user_agent})
+    #puts "code=#{res.code}"    # res.code : Fixnum
+    #puts HTTP::Status.successful?(res.code)
+    #p res.headers
+    res.body
+  end
+end
+
+api = ApiNico.new
+puts api.search_keyword("ゲーム")
